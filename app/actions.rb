ae6f7ec9b0450fa189e086e63cd1472ad6e13e9b
IliaBykov <iliabykov90@gmail.com>
2015-05-25 19:24:39 -0700

    Pages erb.

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,15 @@
 get '/' do
   erb :index #display html in /app/views/index.erb
 end
+
+get '/login.erb' do
+    erb :login
+end
+
+get '/signup.erb' do
+  erb :signup
+end
+
+get '/profile.erb' do
+    erb :profile
+end
